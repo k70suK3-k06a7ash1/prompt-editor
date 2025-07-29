3d080f7f3f299c1c710cfd72598a0e3071ea4c1d
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-07-30 06:17:32 +0900

    chore

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,22 @@ import { Toaster } from "sonner";
 import "./index.css";
 import App from "./App.tsx";
 
+import { PGlite } from "@electric-sql/pglite"
+import { live } from "@electric-sql/pglite/live"
+import { PGliteProvider } from "@electric-sql/pglite-react"
+
+const db = await PGlite.create({
+  extensions: { live }
+})
+
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+        <PGliteProvider db={db}>
+          <App />
+
+        </PGliteProvider>
+		
 		<Toaster />
 	</StrictMode>,
 );
